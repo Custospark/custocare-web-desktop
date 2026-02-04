69d637cef887cdc18a30a9c6ffa3c62295a5a656
OPIYO OSCAR <opiyooscar414@gmail.com>
2026-02-04 20:35:30 +0300

    Remove unused file

--- src/renderer/shared/components/Navigation/Navbar/index.tsx
+++ src/renderer/shared/components/Navigation/Navbar/index.tsx
@@ -6,8 +6,6 @@
 
 export { Navbar as default } from './Navbar';
 export { ContextSwitcher } from './ContextSwitcher';
-export { SmartSearch } from './SmartSearch';
 export { NotificationCenter } from './NotificationCenter';
-export { QuickActions } from './QuickActions';
 export { UserProfileMenu } from './UserProfileMenu';
 export type { NavbarProps } from './Navbar';
